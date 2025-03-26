6571bf3b54aaa85c4db1bc1955f15534fd36e83f
mahmoudmohamed3 <mahmoud2019420@gmail.com>
2025-03-26 06:26:41 +0200

    add polls controller

---
+++ SurveyBasket.Api/Controllers/PollsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SurveyBasket.Api.Controllers
+{
+    [Route("api/[controller]")] // api/polls
+    [ApiController]
+    public class PollsController : ControllerBase
+    {
+
+    }
+}
