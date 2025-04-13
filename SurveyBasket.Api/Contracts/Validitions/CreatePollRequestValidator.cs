2b6358f20b93056bc67e886ca9d4e715928a8806
mahmoudmohamed3 <mahmoud2019420@gmail.com>
2025-04-13 17:53:47 +0200

    Add more Validation & Move Dependincies

--- SurveyBasket.Api/Contracts/Validitions/CreatePollRequestValidator.cs
+++ SurveyBasket.Api/Contracts/Validitions/CreatePollRequestValidator.cs
@@ -1,13 +1,18 @@
-﻿using FluentValidation;
-
-namespace SurveyBasket.Api.Contracts.Validitions
+﻿namespace SurveyBasket.Api.Contracts.Validitions
 {
     public class CreatePollRequestValidator : AbstractValidator<CreatePollRequest>
     {
         public CreatePollRequestValidator()
         {
             RuleFor(x => x.Title)
-                .NotEmpty();
+                .NotEmpty()
+                .WithMessage("Please add a {PropertyName}")
+                .Length(3, 100)
+                .WithMessage("Title Should be at least {MinLength} and maximum {MaxLength}, you entered [{TotalLength}]");
+
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .Length(3, 1000);
         }
     }
 }
--- SurveyBasket.Api/Program.cs
+++ SurveyBasket.Api/Program.cs
@@ -1,31 +1,13 @@
 using Mapster;
 using MapsterMapper;
 using SharpGrip.FluentValidation.AutoValidation.Mvc.Extensions;
+using SurveyBasket;
 using System.Reflection;
 
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-// using Mapster
-var mappingConfig = TypeAdapterConfig.GlobalSettings;
-mappingConfig.Scan(Assembly.GetExecutingAssembly());
-
-builder.Services.AddSingleton<IMapper>(new Mapper(mappingConfig));
-
-// Deprndency Injection
-builder.Services.AddScoped<IPollService, PollService>();
-
-// Fluent Validation Package
-builder.Services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
-    .AddFluentValidationAutoValidation();
+builder.Services.AddDependencies();
 
 var app = builder.Build();
 
