5731a95c437ba87167011cc9098b4fee59e55f99
mahmoudmohamed3 <mahmoud2019420@gmail.com>
2025-04-24 17:54:09 +0200

    Section 6: Add Dates Validations

--- SurveyBasket.Api/Contracts/Validitions/PollRequestValidator.cs
+++ SurveyBasket.Api/Contracts/Validitions/PollRequestValidator.cs
@@ -13,8 +13,25 @@
             RuleFor(x => x.Summary)
                 .NotEmpty()
                 .Length(3, 1500);
-    
+
+            RuleFor(x => x.StartsAt)
+                .NotEmpty()
+                .GreaterThanOrEqualTo(DateOnly.FromDateTime(DateTime.Today));
+
+            RuleFor(x => x.EndsAt)
+                .NotEmpty();
+
+            RuleFor(x => x)
+                .Must(HasValidDates)
+                .WithName(nameof(PollRequest.EndsAt))
+                .WithMessage("{PropertyName} must be greater than or equals Start date");
+
+
         }
-       
+
+        public bool HasValidDates(PollRequest poll)
+        {
+            return poll.EndsAt >= poll.StartsAt;
+        }
     }
 }
